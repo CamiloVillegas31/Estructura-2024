62a14b1909e692a4c8d7f4675b14b4f2b038420d
Camilo Andres Villegas Flor <162384786+CamiloVillegas31@users.noreply.github.com>
2024-11-05 08:42:06 -0500

    cambios

---
+++ EjercicioCola/src/ejerciciocola/EjercicioCola.java
@@ -0,0 +1,26 @@
+/* 
+Integrantes
+
+Daniel Estiben Medina Tobar
+Camilo Andrés Villegas Flor
+
+Corporación Universitaria Unicomfacauca.
+Ing. de sistemas-Diurno
+
+ */
+package ejerciciocola;
+
+/**
+ *
+ * @author User
+ */
+public class EjercicioCola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
